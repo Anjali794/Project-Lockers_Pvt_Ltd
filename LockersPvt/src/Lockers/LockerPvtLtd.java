09b0ff2428a667b7c39268f21cf0aa879191d5a9
Anjali794 <53463182+Anjali794@users.noreply.github.com>
2022-03-10 07:54:47 -1200

    Update LockerPvtLtd.java

--- LockersPvt/src/Lockers/LockerPvtLtd.java
+++ LockersPvt/src/Lockers/LockerPvtLtd.java
@@ -3,93 +3,47 @@ package Lockers;
 import java.util.Scanner;
 
 class LockerPvtLtd {
+	public static void main(String[] args) {
 
-    public static void main(String[] args) {
-        int ch=0, choice=0;
-        Scanner sc =new Scanner(System.in);
+        System.out.println("**********************");
+        System.out.println("Welcome to LOCK ME :) ");
+        System.out.println(" LockerMe.com");
+        System.out.println("Develop by Anjali");
+        System.out.println("**********************");
+        
+ Scanner sc=new Scanner(System.in);
+        BusinessOperations.mainMenu();
+        while(true) {
+            
+            
+                try {
+        	
+              int option=sc.nextInt();
+              switch(option) {
+                case 1 :// listing the files in ascending order.
+                    System.out.println("These are the files in ascending");
+                    BusinessOperations.listingFile();
+                    break;
+                case 2 :
+                	BusinessOperations.fileMenuOperations();
+                	break;
+                    
+                case 3 :
+            	   System.out.println("Thank you for using Lockers, Have a nice day!! ");
+                   System.out.println("Application closed");
 
-        System.out.println("\t**********************\n");
-        System.out.println("\t Welcome to LOCK IT! ");
-        System.out.println("\t By, Locker Pvt Ltd. \n");
-        System.out.println("\t**********************");
-        System.out.println(" Developer\t: Anjali Verma \n ");
+                   System.exit(option);
+                    break;
+                default:
+                    System.out.println(" Invalid Input !! please enter valid number");
+                    break;
 
-        while(true)
-        {
-            System.out.println("Please choose one of the following options :");
-            System.out.println("1. List Current Files");
-            System.out.println("2. Business Operations");
-            System.out.println("3. Close Application");
-            try{
-                ch = sc.nextInt();
             }
-            catch(Exception e)
-            {
-                System.out.println("Null Exception occurred");
-            }
-
-            switch (ch) {
-                case 1 -> //list all files in ascending order.
-                        BusinessOperations.listFiles();
-                case 2 -> {
-                    System.out.println("Please choose one of the following options :");
-                    System.out.println("1. Add a File");
-
-                    System.out.println("2. Delete a File");
-                    System.out.println("3. Search for a File");
-                    System.out.println("4. Go Back");
-                    try {
-                        choice = sc.nextInt();
-                    } catch (Exception e) {
-                        System.out.println("Null Exception occurred");
-                    }
-                    switch (choice) {
-                        case 1 -> {
-                            //Creation of a file takes place
-                            System.out.println("Enter the name of a file to be created: ");
-                            String fileCreate = sc.next();
-
-                            // Calling the function to create the file
-                            BusinessOperations.createFile(fileCreate);
-                        }
-                        case 2 -> {
-                            //deletion of a file takes place
-                            System.out.print("Enter the name of a file to be deleted: ");
-                            String fileDelete = sc.next();
-
-                            // Calling the function to delete the file
-                            BusinessOperations.deleteFile(fileDelete);
-                        }
-                        case 3 -> {
-                            //Search for a file takes place
-                            System.out.println("Enter the name of a file to be searched: ");
-                            String fileSearch = sc.next();
-
-                            // Calling the function to search the file
-                            BusinessOperations.searchFile(fileSearch);
-                        }
-                        case 4 -> {
-                            // go back to Main Menu
-                            
-                        }
-                        default ->
-                                //In the case of unprecedented input execute this
-                                System.out.println("\n Invalid Input Given, Please Repeat the process.\n");
-                    }
-                }
-                case 3 -> {
-
-                    //Voluntarily exiting the application
-                    sc.close();
-                    System.out.println("\n It was nice having you here! Thank you..");
-                    System.exit(1);
-                }
-                default ->
-                        //In the case of unprecedented input execute this
-                        System.out.println("\n\n Invalid Input! Please Select within the given range.\n");
-            }
-        }
-
-    }
+        }catch(Exception e){
+          System.out.println("Invalid Input !! Please Enter valid number");
+          LockerPvtLtd.main(args);
+     }
+}
+}
 
 }
